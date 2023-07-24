47c5e69746f658ea370dc6f915cb8cfc8297e7a0
daniel7491 <dnai745@gmail.com>
2023-07-24 20:49:30 +0300

    adding aritmetic

--- lesson_01_variables/01_input_examples/04_input_map.py
+++ lesson_01_variables/01_input_examples/04_input_map.py
@@ -3,3 +3,4 @@ score_1, score_2, score_3 = map(int, input("enter 3 scores: ").split())
 print(score_1 + score_2 + score_3)
 #with map you can add more
 
+# https://s3.eu-west-1.amazonaws.com/data.cyber.org.il/virtual_courses/next.py/data/chapter_1/graphics/img_1.1_1.gif - what map does
