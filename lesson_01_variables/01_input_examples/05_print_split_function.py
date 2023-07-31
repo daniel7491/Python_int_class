9feac1746f69205f60009551dc42f22cf5d8d3f5
daniel7491 <dnai745@gmail.com>
2023-07-31 18:30:49 +0300

    adding 5 and 6

---
+++ lesson_01_variables/01_input_examples/05_print_split_function.py
@@ -0,0 +1,6 @@
+print(1, 2, 3, 4, 5)
+print(1, 2, 3, 4, 5, sep=' ')
+print(1, 2, 3, 4, 5, sep='')
+print(1, 2, 3, 4, 5, sep='\t')
+print(1, 2, 3, 4, 5, sep='\n')
+print(1, 2, 3, 4, 5, sep='####') # sep sperates the stuff you want to print or puts whatever you want in between them
