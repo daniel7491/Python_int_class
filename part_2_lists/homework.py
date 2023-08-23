3b19c601ae350ff081489e2a62c1ec9a8012c6e1
dani7491 <dnai745@gmail.com>
2023-08-23 14:54:24 +0300

    first list homework

---
+++ part_2_lists/homework.py
@@ -0,0 +1,12 @@
+users = []
+users.append("kevin")
+users.append("bob")
+users.append("alice")
+print(users)
+del users[1:2]
+print(users)
+revers_users = list(reversed(users))
+print(revers_users)
+users.insert(1,"melody")
+print(users)
+users = users + "asdsad" "asdasd "
