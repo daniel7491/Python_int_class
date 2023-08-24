6da03cff38b0c6064a1ea02425c2a4ab4cfed0a7
dani7491 <dnai745@gmail.com>
2023-08-24 10:01:44 +0300

    adding last

--- part_2_lists/homework.py
+++ part_2_lists/homework.py
@@ -9,4 +9,7 @@ revers_users = list(reversed(users))
 print(revers_users)
 users.insert(1,"melody")
 print(users)
-users = users + "asdsad" "asdasd "
+users = users + ['andy', 'wanda', 'jim']
+print(users)
+sliced_users= users[2:-2]
+print(sliced_users)
