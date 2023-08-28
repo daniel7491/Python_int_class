0efae3ddf120ae9f672828e6769f5ede31a69fa1
dani7491 <dnai745@gmail.com>
2023-08-28 18:45:37 +0300

    adding tuple

---
+++ tuples/01.py
@@ -0,0 +1,15 @@
+p = (1,2,3,4,5)
+print(f"{type(p)}")
+print(f"{p= }")
+print(f"{p[0]=}")
+
+
+
+point_3d = (1.2, 4.4,21)
+x, y, z = point_3d
+
+print(f"{x}=")
+print(f"{y}=")
+print(f"{z}=")
+
+print(point_3d[::-1]) # print in reverse
---
+++ tuples/practice/01.py
@@ -0,0 +1,5 @@
+x = int(input())
+y = int(input())
+
+my_tuple = range(x,y+1)
+print(tuple(my_tuple))
